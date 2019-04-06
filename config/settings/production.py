d8f26ff418580e0d4700b67dfbeab9578d39b094
Brandon <brandon.haw@gmail.com>
2019-04-06 14:01:13 -0400

    Changed allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,7 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["connectswell.com", "blossome-app.herokuapp.com"])
-
+ALLOWED_HOSTS += "blossome-app.herokuapp.com"
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
