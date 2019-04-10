2f80fce89a09c5476d3b053765a146544c219082
Brandon <brandon.haw@gmail.com>
2019-04-10 19:37:03 -0400

    More style changes

--- config/urls.py
+++ config/urls.py
@@ -6,8 +6,8 @@ from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
 urlpatterns = [
-                  path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-                  path("index/", TemplateView.as_view(template_name="pages/index.html"), name="index"),
+                  # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+                  path("", TemplateView.as_view(template_name="pages/index.html"), name="index"),
                   path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
                   # Django Admin, use {% url 'admin:index' %}
                   path(settings.ADMIN_URL, admin.site.urls),
